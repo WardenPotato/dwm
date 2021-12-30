a75067fd51f882a62e18f601396fd78578c0efba
WardenPotato <warden@localdomain>
2021-12-30 04:35:32 +0100

    - Config edits

--- config.def.h
+++ config.def.h
@@ -59,11 +59,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *flameshotcmd[]  = { "flameshot", "gui", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Print,  spawn,          {.v = flameshotcmd } },
+	{ MODKEY,                       XK_p,      spawn,          SHCMD("echo -e \"poweroff\nreboot\nsuspend\" | dmenu -p \"Power Options\" | xargs systemctl") },
+	{ MODKEY,                       XK_v,      spawn,          SHCMD("clipmenu") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_d,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_a,      focusstack,     {.i = -1 } },
